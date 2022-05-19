b420f04632fb8d1b5c781838d37eae262424bc8b
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-19 15:23:51 +0200

    router react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React, { FunctionComponent } from "react";
 import PokemonList from "./pages/pokemon-list";
+import PokemonDetail from "./pages/pokemon-detail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 //OR
 // import React from "react";
 
 // const App: React.FC = () => {
 const App: FunctionComponent = () => {
-  return <PokemonList />;
+  return (
+    <Router>
+      <div>
+        {/* la barre de navigation à toutes les pages */}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              Pokédex
+            </Link>
+          </div>
+        </nav>
+        {/* le systeme de gestion des routes de notre app */}
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemons/:id" component={PokemonDetail} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
