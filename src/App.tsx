59538640e21179bbfeeb7993faf8707898ea45b5
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-19 11:56:12 +0200

    second composants

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import Pokemon from "./models/pokemon";
-import POKEMONS from "./models/mock-pokemon";
-
-import React, { FunctionComponent, useState, useEffect } from "react";
+import React, { FunctionComponent } from "react";
+import PokemonList from "./pages/pokemon-list";
 //OR
 // import React from "react";
 
 // const App: React.FC = () => {
 const App: FunctionComponent = () => {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-  useEffect(() => {
-    setPokemons(POKEMONS);
-  }, []);
-
-  return (
-    <div>
-      <h1> Pokedex</h1>
-      <p>il y a {pokemons.length} pokemons dans ce pokedex</p>
-    </div>
-  );
+  return <PokemonList />;
 };
 
 export default App;
