85122d42f2e02b20afc5255f2bc56521bdcd51d4
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-19 08:58:52 +0200

    first commiut

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import React, { FunctionComponent } from "react";
+import React from "react";
+//const App: FunctionComponent = () => {
+const App: React.FC = () => {
+  const name: String = "React";
+
+  return <h1>Hello, {name} !</h1>;
+};
+
+export default App;
