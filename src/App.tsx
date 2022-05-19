216894946ddc837e2a4ebd7185b709daf8dc63cb
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-19 09:35:31 +0200

    first composant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-// import React, { FunctionComponent } from "react";
-import React from "react";
-//const App: FunctionComponent = () => {
-const App: React.FC = () => {
-  const name: String = "React";
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
 
-  return <h1>Hello, {name} !</h1>;
+import React, { FunctionComponent, useState } from "react";
+//OR
+// import React from "react";
+
+// const App: React.FC = () => {
+const App: FunctionComponent = () => {
+  const [pokemons] = useState<Pokemon[]>(POKEMONS);
+
+  return (
+    <div>
+      <h1> Pokedex</h1>
+      <p>il y a {pokemons.length} pokemons dans ce pokedex</p>
+    </div>
+  );
 };
 
 export default App;
